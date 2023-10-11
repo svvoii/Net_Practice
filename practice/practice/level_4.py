eca420f1b4815a51f33f6571237e9c2340030f54
Serge <serge@Serges-MacBook-Pro.local>
2023-10-11 07:19:37 +0200

    to fill in level_3 to 5

---
+++ practice/practice/level_4.py
@@ -0,0 +1,38 @@
+# Level 4:
+
+# Task description:
+#
+# Goal 1: ""
+
+# Given network settings parameters:
+client_A = {
+    "name": "",
+    "interface": "",
+    "IP": "",
+    "Mask": ""
+}
+
+client_B = {
+    "name": "",
+    "interface": "",
+    "IP": "",
+    "Mask": ""
+}
+
+client_C = {
+    "name": "",
+    "interface": "",
+    "IP": "",
+    "Mask": ""
+}
+
+switch_S = {
+    "name": "",
+    "interface": "",
+    "IP": "",
+    "Mask": ""
+}
+
+# Modify the following parameters to achieve the Goal :
+client_C["IP"] = ""
+client_D["IP"] = ""
