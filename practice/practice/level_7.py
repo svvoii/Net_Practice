7558fe484e33dad16c560fbae81fd4ece2cbd834
Sergiu BOCANCIA <sbocanci@made-f0Br7s1.clusters.42paris.fr>
2023-10-13 19:35:56 +0200

    done with lvl 8, the most fun starts now

--- practice/practice/level_7.py
+++ practice/practice/level_7.py
@@ -43,7 +43,7 @@ router_R2 = {
 			"Mask": ""
 		},
 		{
-			"interface": "R12",
+			"interface": "R22",
 			"IP": "",
 			"Mask": ""
 		}
@@ -64,24 +64,51 @@ client_C = {
 """
 The following parameters need to be added to achieve the Goal :
 """
-client_A["IP"] = ""
-client_A["Mask"] = ""
-client_A["Destination"] = ""
-client_A["Next hop"] = ""
+client_A["IP"] = "101.198.14.2"
+client_A["Mask"] = "255.255.255.192" # or "/26"
+client_A["Destination"] = "0.0.0.0/0" # or "default"
+client_A["Next hop"] = "101.198.14.1" # interface of the nearest connected router
 
-router_R1["interfaces"][0]["Mask"] = ""
-router_R1["interfaces"][1]["Mask"] = ""
-router_R1["Destination"] = ""
-router_R1["Next hop"] = ""
+router_R1["interfaces"][0]["Mask"] = "255.255.255.192" # or "/26"
+router_R1["interfaces"][1]["Mask"] = "255.255.255.128" # also can be "/25" or higher but not lower since there should be roo m for at least 3 sub-networks
+router_R1["Destination"] = "0.0.0.0/0" # or "default"
+router_R1["Next hop"] = "101.198.14.253"
 
-router_R2["interfaces"][0]["IP"] = ""
-router_R2["interfaces"][0]["Mask"] = ""
-router_R2["interfaces"][1]["IP"] = ""
-router_R2["interfaces"][1]["Mask"] = ""
-router_R2["Destination"] = ""
-router_R2["Next hop"] = ""
+router_R2["interfaces"][0]["IP"] = "101.198.14.253"
+router_R2["interfaces"][0]["Mask"] = "255.255.255.128" # should be the same as R1 mask
+router_R2["interfaces"][1]["IP"] = "101.198.14.65" # can bi in the range from -.-.-.65 to 126, according to subnet mask /26
+router_R2["interfaces"][1]["Mask"] = "255.255.255.192" # or "/26" also can be /27, /28, /29, /30 but not smaler that /26
+router_R2["Destination"] = "101.198.14.254"
+router_R2["Next hop"] = "0.0.0.0/0"
 
-client_C["IP"] = ""
-client_C["Mask"] = ""
-client_C["Destination"] = ""
-client_C["Next hop"] = ""
+client_C["IP"] = "101.198.14.66" # can bi in the range from -.-.-.65 to 126, according to subnet mask /26
+client_C["Mask"] = "255.255.255.192" # or "/26"
+client_C["Destination"] = "0.0.0.0/0" # or "default"
+client_C["Next hop"] = "101.198.14.65" # interface of the nearest connected router
+
+"""
+Given network layout in this task there should have at least 3 subnetworks to manage.
+1st subnet: router_R1's interface R11 and client_A's interface
+2nd subnet: router_R2's interface R22 and client_C's interface
+3rd subnet: router_R1's interface R12 and router_R2's interface R21
+
+Since the task does not precise any subnet mask it is required to chose the proper one.
+So, we can check the subnetting table to identify which subnet mask can handle at least 3 subnets
+in this case the '/26' or '255.255.255.192` submask can handle up to 4 subnets with the folowing 
+IP addresses range:
+-.-.-.1 - 62
+-.-.-.65 - 126
+-.-.-.129 - 190
+-.-.-.193 - 254
+
+It is also possible to use other subnet masks for subnets with clients.
+In this case those subnet masks should be higher in value than subnet where the both routers are located.
+If routers are in '/26's subnet the clients could be in '/27', '/28', '/29' subnet with appropriate 
+IP addresses range.
+
+Filling in the IPs we need to keep in mind that connected interfaces should be within respective IP range
+chosen for that subnet
+
+The routing table in this task is more straight forward since it is not necessary to use 
+specific destination, 'defailt' / '0.0.0.0/0' value would be sufficient.
+"""
--- practice/practice/level_8.py
+++ practice/practice/level_8.py
@@ -1,1 +1,79 @@
 # Level 8:
+
+# Network layout / topology:
+internet_I = {
+	"name": "Internet",
+	"Destination": "132.251.9.0/26",
+	"Next hop": ""
+}
+
+router_R1 = {
+    "name": "gate.non-real.com",
+	"interfaces": [
+		{
+			"interface": "R12",
+			"IP": "163.114.250.12",
+			"Mask": "255.255.255.240"
+		},
+		{
+			"interface": "R13",
+			"IP": "",
+			"Mask": ""
+		}
+	],
+	"routing table": [
+		{
+			"Destination": "",
+			"Next hop": ""
+		},
+		{
+			"Destination": "0.0.0.0/0",
+			"Next hop": "163.114.250.1"
+		}
+	]
+}
+
+router_R2 = {
+    "name": "transit.my-isp.org",
+	"interfaces": [
+		{
+			"interface": "R21",
+			"IP": "",
+			"Mask": ""
+		},
+		{
+			"interface": "R22",
+			"IP": "",
+			"Mask": ""
+		},
+		{
+			"interface": "R23",
+			"IP": "",
+			"Mask": ""
+		}
+	],
+	"Destination": "",
+	"Next hop": "132.251.9.62"
+}
+
+client_C = {
+    "name": "office.non-real.com",
+    "interface": "C1",
+    "IP": "",
+    "Mask": "",
+	"Destination": "",
+	"Next hop": ""
+}
+
+client_D = {
+    "name": "home.non-real.com",
+    "interface": "D1",
+    "IP": "",
+    "Mask": "255.255.255.240",
+	"Destination": "",
+	"Next hop": ""
+}
+# topology description:
+
+
+# Task description:
